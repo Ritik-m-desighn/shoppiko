99619f459e5152699643a2f3717efef6894d6d3d
Ritik-m-desighn <srivastavritik0342gmail.com>
2025-12-23 00:21:13 +0530

    Rename order.js to Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -12,7 +12,7 @@ const orderSchema = new mongoose.Schema({
     {
       name: { type: String, required: true },
       quantity: { type: Number, required: true },
-      imageUrl: { type: String, required: true },
+      imageUrl: { type: String, required: true },  
       price: { type: Number, required: true },
       product: {
         type: mongoose.Schema.Types.ObjectId,
